0c0710817b0642ac4f27b2cbd5fd939f07f0cd41
Matthew Larby <37371392+mattl1598@users.noreply.github.com>
2022-05-13 23:05:00 +0100

    fixed thing

--- js/chat2.js
+++ js/chat2.js
@@ -109,9 +109,12 @@ function parseMsg(message){
     //     message.data.redemption.reward.title === "Word of the Day" &&
     //     message.data.redemption.status === "FULFILLED"
     // ) {
-        let word = message
-        // let word = message.data.redemption.user_input.split(" ");
-        document.querySelector("#word").innerText = word.replace(/<[^>]*>?/gm, '');
+        let word = message.replace(/<[^>]*>?/gm, '')
+        // let word = message.data.redemption.user_input.split(" ")[0].replace(/<[^>]*>?/gm, '');
+        $("#word").text(word);
+        $("#word").css({
+            fontSize: "calc(100vw/"+word.length+")",
+        });
     // }
 }
 
